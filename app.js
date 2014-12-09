8e23d1d4371d09105ed3658d7fe45ed09604c08c
cjob <christophe.job@oracle.com>
2014-12-09 12:12:49 -0800

    Got the Restful server limping

--- app.js
+++ app.js
@@ -2,18 +2,27 @@ var express = require('express');
 var cors = require('cors');
 var app = express();
 
+var Workers = [
+  	{ first_name : 'Christophe' , last_name : 'job' , email_address : 'christophe.job@oracle.com' , manager_email_address : 'thomas.kurian@oracle.com' },
+	{ first_name : 'Peter' , last_name : 'Magnussen' , email_address : 'psm@oracle.com' , manager_email_address : 'thomas.kurian@oracle.com' },
+    { first_name : 'Thomas' , last_name : 'Kurian' , email_address : 'thomas.kurian@oracle.com' , manager_email_address : 'larry.ellison@oracle.com' }
+]  
+
 app.use(cors());
 
-
-app.get('/crm.yaml', function(req, res) {
-    res.sendfile('./public/crm.yaml');
-});
-
-app.get('/swagger.json', function(req, res) {
-    res.sendfile('./public/swagger.json');
-});
  
+app.get('/v1/Workers', function (req, res) {
+    var retval = [];
+        r = 0;
+	for (i =0 ; i < Workers.length ; i ++) {
 
-// app.use(express.static(__dirname + '/public'));
+		if (req.query.email_address == Workers[i].email_address)
+			retval[r] = Workers[i];
+		    r++;
+	}
+	res.json(retval);
+})
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT || 3000);
