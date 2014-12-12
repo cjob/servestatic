adca66056a7fe3fe69e0da7b829a46d605827b82
cjob <christophe.job@oracle.com>
2014-12-12 09:43:52 -0800

    Added wrokers

--- app.js
+++ app.js
@@ -5,21 +5,27 @@ var app = express();
 var Workers = [
   	{ first_name : 'Christophe' , last_name : 'job' , email_address : 'christophe.job@oracle.com' , manager_email_address : 'thomas.kurian@oracle.com' },
 	{ first_name : 'Peter' , last_name : 'Magnussen' , email_address : 'psm@oracle.com' , manager_email_address : 'thomas.kurian@oracle.com' },
-    { first_name : 'Thomas' , last_name : 'Kurian' , email_address : 'thomas.kurian@oracle.com' , manager_email_address : 'larry.ellison@oracle.com' }
-]  
+    { first_name : 'Thomas' , last_name : 'Kurian' , email_address : 'thomas.kurian@oracle.com' , manager_email_address : 'larry.ellison@oracle.com' },
+    { first_name : 'Campbell' , last_name : 'Webb' , email_address : 'campbell.webb@oracle.com' , manager_email_address : 'thomas.kurian@oracle.com' },
+    { first_name : 'Gopi' , last_name : 'Tummala' , email_address : 'gopi.tummala@oracle.com' , manager_email_address : 'psm@oracle.com' },
+    { first_name : 'Jacky' , last_name : 'Abella' , email_address : 'jacky.dominguez@oracle.com' , manager_email_address : 'gopi.tummala@oracle.com' }
+]
 
 app.use(cors());
 
  
 app.get('/v1/Workers', function (req, res) {
+    console.log('request: ',req.url);
     var retval = [];
         r = 0;
 	for (i =0 ; i < Workers.length ; i ++) {
 
-		if (Workers[i].email_address.search(req.query.email_address) != -1)
-			retval[r] = Workers[i];
-		    r++;
+		if (Workers[i].email_address.search(req.query.email_address) != -1) {
+            retval[r] = Workers[i];
+            r++;
+        }
 	}
+
 	res.json(retval);
 })
 
